3657f5c3840c270a6e45ffc24a6894d5840eaf55
Daniel <danielstarling4@gmail.com>
2019-06-05 14:53:21 -0700

    basic plan complete, start of function worked out.

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,8 +2,113 @@
 
 import math
 
+"""
+Understand
+-------------
+- function recieves a recipe {dictionary}
+- function also recieves all the ingredients available to us {dictionary}
+- Keys are ingredient names - values are corresponding amounts
+- In ingredients dictionary value represent amount available to us
+
+- Output the maximum number of whole batches that can be made with the
+- supplied recipe using the ingredients available. 
+
+Plan
+-------------
+
+- loop through recipes
+- nested loop through ingredients
+- check for name in ingredients list
+- compare the value
+- if there is enough continue, if there is not, break
+- if the whole loop runs, count 1 then run it again
+- when it runs out of ingredients break and return the count
+
+
+Carry Out Plan
+--------------
+
+
+"""
+
 def recipe_batches(recipe, ingredients):
-  pass 
+	quantity = None
+	
+	# print(recipe.items()) #prints keys and values
+	for item in recipe.items():
+		# print(item)
+		for j in ingredients.items():
+
+			# print("j",j[1])
+			if item[0] in ingredients and item[1] <= j[1]:
+				print(item)
+				# print("ingredients",j)
+
+
+
+
+	# return quantity
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+recipe_batches(
+  { 'milk': 100, 'butter': 50, 'flour': 5 },
+  { 'milk': 138, 'butter': 48, 'flour': 51 }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
